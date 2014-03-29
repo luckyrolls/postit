163d209b60ab0d9508708c45ec5644f2bc8dae1b
luckyrolls <markmun99@gmail.com>
2014-03-29 16:09:28 +0000

    ongoing

---
+++ db/migrate/20140321053403_add_user_name_column_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUserNameColumnToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :username, :string
+  end
+end
---
+++ db/migrate/20140323222959_remove_user_name_column_to_posts.rb
@@ -0,0 +1,5 @@
+class RemoveUserNameColumnToPosts < ActiveRecord::Migration
+  def change
+    remove_column :posts, :username, :string
+  end
+end
